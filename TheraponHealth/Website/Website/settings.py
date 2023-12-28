9f103800d40d0d71e2317d22056368dbd7020ecf
Dylan Edwards <dylan.edwards90@outlook.com>
2023-12-28 16:24:18 -0500

    Update

--- TheraponHealth/Website/Website/settings.py
+++ TheraponHealth/Website/Website/settings.py
@@ -130,9 +130,9 @@ STORAGES = {
     "default": {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
-    "staticfiles": {
-        # "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
+    # "staticfiles": {
+    #     # "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    # },
 }
 
 # Default primary key field type
